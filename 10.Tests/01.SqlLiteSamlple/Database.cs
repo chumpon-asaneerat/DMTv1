a329603d5779822d0603c6c8ec34192a44127830
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-05 05:49:39 +0700

    Update 2020-06-05 #6 - Update SQLLite sample code. - Add SQLLite sample project post build script. (required).

---
+++ 10.Tests/01.SqlLiteSamlple/Database.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+using System.Data.SQLite;
+
+namespace SqlLiteSamlple
+{
+    public class Database
+    {
+        public string FileName { get; set; }
+
+        private string ConnectionString
+        {
+            get { return "Data Source=" + FileName + ";Version=3"; }
+        }
+
+        public void Create()
+        {
+            string path = Path.Combine("./", FileName);
+            if (!File.Exists(path))
+            {
+                SQLiteConnection.CreateFile(path);
+                using (SQLiteConnection conn = new SQLiteConnection(ConnectionString))
+                {
+                    conn.Open();
+
+                    SQLiteCommand cmd = new SQLiteCommand(conn);
+                    string query = @"CREATE TABLE LOGIN(UserName Text(25), Password Text(25));";
+                    cmd.CommandText = query;
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+                    cmd = null;
+
+                    conn.Close();
+                }
+            }
+        }
+
+        public void Add(string UserName, string Password)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(ConnectionString))
+            {
+                conn.Open();
+
+                SQLiteCommand cmd = new SQLiteCommand(conn);
+                string query = @"INSERT INTO LOGIN(UserName, Password) VALUES(@username, @password);";
+                cmd.CommandText = query;
+                cmd.Parameters.Add(new SQLiteParameter("@username", UserName));
+                cmd.Parameters.Add(new SQLiteParameter("@password", Password));
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                cmd = null;
+
+                conn.Close();
+            }
+        }
+
+        public DataTable GetAll()
+        {
+            DataTable result = new DataTable();
+            using (SQLiteConnection conn = new SQLiteConnection(ConnectionString))
+            {
+                conn.Open();
+
+                SQLiteCommand cmd = new SQLiteCommand(conn);
+                string query = @"SELECT * FROM LOGIN";
+                cmd.CommandText = query;
+                SQLiteDataAdapter adaptor = new SQLiteDataAdapter();
+
+                adaptor.SelectCommand = cmd;
+                adaptor.Fill(result);
+
+                adaptor.Dispose();
+                adaptor = null;
+                cmd.Dispose();
+                cmd = null;
+
+                conn.Close();
+            }
+            return result;
+        }
+    }
+}
--- 10.Tests/01.SqlLiteSamlple/Form1.cs
+++ 10.Tests/01.SqlLiteSamlple/Form1.cs
@@ -16,5 +16,30 @@ namespace SqlLiteSamlple
         {
             InitializeComponent();
         }
+
+        private Database db = new Database();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            db.FileName = "sample.db";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create
+            db.Create();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Add
+            db.Add(txtUserName.Text, txtPassword.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Query
+            dataGridView1.DataSource = db.GetAll();
+        }
     }
 }

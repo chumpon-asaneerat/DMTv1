e45bcc684d0a9229a38c1217d270e62ef5a07650
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-04 12:39:44 +0700

    Update 2020-06-04 #3 - DMT.Models add DMTModelBase abstract class.

---
+++ 01.Core/02.DMT.Models/Models/DMTModelBase.cs
@@ -0,0 +1,48 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using NLib;
+using DMT;
+
+// required for JsonIgnore attribute.
+using Newtonsoft.Json;
+
+#endregion
+
+namespace DMT.Models
+{
+    #region DMTModelBase (abstract)
+
+    /// <summary>
+    /// The DMTModelBase abstract class.
+    /// Provide basic implementation of INotifyPropertyChanged interface.
+    /// </summary>
+    public abstract class DMTModelBase : INotifyPropertyChanged
+    {
+        #region Private Methods
+
+        /// <summary>
+        /// Raise Property Changed Event.
+        /// </summary>
+        /// <param name="propertyName">The property name.</param>
+        protected void RaiseChanged(string propertyName)
+        {
+            PropertyChanged.Call(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        #region Public Events
+
+        /// <summary>
+        /// The PropertyChanged event.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+
+    #endregion
+}

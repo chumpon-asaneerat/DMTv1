107dcc9f2be330cecdf616bdbb14c3968624b917
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-07 03:51:59 +0700

    Update 2020-06-07 #1 - Update sample for LiteDB.

--- 10.Tests/02.LiteDBSample/Form1.Designer.cs
+++ 10.Tests/02.LiteDBSample/Form1.Designer.cs
@@ -28,13 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 151);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(696, 333);
+            this.dataGridView1.TabIndex = 23;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 113);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 32);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Query";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(173, 76);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(155, 22);
+            this.txtPassword.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(170, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Password:";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(12, 76);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(155, 22);
+            this.txtUserName.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "User Name:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(334, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 32);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 32);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Create/Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(452, 67);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 32);
+            this.button4.TabIndex = 24;
+            this.button4.Text = "Add 100000";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(151, 113);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 32);
+            this.button5.TabIndex = 25;
+            this.button5.Text = "Delete All";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(720, 496);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- 10.Tests/02.LiteDBSample/Form1.cs
+++ 10.Tests/02.LiteDBSample/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LiteDB;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,97 @@ namespace LiteDBSample
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        // Create your POCO class entity
+        public class User
+        {
+            //public int Id { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+            //public string[] Phones { get; set; }
+            //public bool IsActive { get; set; }
+        }
+
+        private LiteDatabase db = null;
+        private ILiteCollection<User> users = null;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (null != db)
+            {
+                db.Dispose();
+            }
+            db = null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create connection
+            if (null == db)
+            {
+                db = new LiteDatabase(@".\users.db");
+                // Get a collection(or create, if doesn't exist)
+                users = db.GetCollection<User>("users");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (null == users) return;
+            // Add
+            var user = new User
+            {
+                UserName = txtUserName.Text,
+                Password = txtPassword.Text
+            };
+            users.Insert(user);
+            users.EnsureIndex(x => x.UserName);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (null == users) return;
+            // Use LINQ to query documents (filter, sort, transform)
+            var results = users.Query()
+                //.Where(x => x.UserName.StartsWith("J"))
+                //.OrderBy(x => x.UserName)
+                //.Select(x => new { x.UserName, NameUpper = x.UserName.ToUpper() })
+                //.Limit(10)
+                .ToList();
+            dataGridView1.DataSource = results;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (null == users) return;
+            for (int i = 0; i < 100000; i++)
+            {
+                // Add
+                var user = new User
+                {
+                    UserName = txtUserName.Text + i.ToString("D4"),
+                    Password = txtPassword.Text
+                };
+                users.Insert(user);
+            }
+            users.EnsureIndex(x => x.UserName);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (null == users) return;
+            users.DeleteAll();
+
+            db.Rebuild(); // shrink database size.
+            // Get a collection(or create, if doesn't exist)
+            users = db.GetCollection<User>("users");
         }
     }
 }

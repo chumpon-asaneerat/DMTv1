07879192b1d58f145c33718e0f0b9315bec23568
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-08 21:43:31 +0700

    Update 2020-06-08 #8 - Add TA - CollectorFundSummaryView control. - Add TA - CollectorCouponBorrowEntry control. - Add TA - CollectorCouponReturnEntry control.

---
+++ 02.Controls/01.DMT.Controls/Controls/TA/Collector/Coupon/CollectorCouponReturnEntry.xaml.cs
@@ -0,0 +1,43 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+#endregion
+
+namespace DMT.Controls
+{
+    /// <summary>
+    /// Interaction logic for CollectorCouponReturnEntry.xaml
+    /// </summary>
+    public partial class CollectorCouponReturnEntry : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public CollectorCouponReturnEntry()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ 02.Controls/01.DMT.Controls/Controls/TA/Collector/Fund/CollectorFundSummaryView.xaml.cs
@@ -0,0 +1,43 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+#endregion
+
+namespace DMT.Controls
+{
+    /// <summary>
+    /// Interaction logic for CollectorFundSummaryView.xaml
+    /// </summary>
+    public partial class CollectorFundSummaryView : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public CollectorFundSummaryView()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}

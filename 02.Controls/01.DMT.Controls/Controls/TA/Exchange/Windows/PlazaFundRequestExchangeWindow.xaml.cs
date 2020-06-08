e6c4f1bb9ab70f1ae8bb823151027e10e21f2906
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-08 21:58:20 +0700

    Update 2020-06-08 #10 - Add TA - PlazaFundRequestExchangeWindow (window). - Add TA - PlazaFundUpdateExchangeWindow (window).

---
+++ 02.Controls/01.DMT.Controls/Controls/TA/Exchange/Windows/PlazaFundRequestExchangeWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+#endregion
+
+namespace DMT.Windows
+{
+    /// <summary>
+    /// Interaction logic for PlazaFundRequestExchangeWindow.xaml
+    /// </summary>
+    public partial class PlazaFundRequestExchangeWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PlazaFundRequestExchangeWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
---
+++ 02.Controls/01.DMT.Controls/Controls/TA/Exchange/Windows/PlazaFundUpdateExchangeWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+#endregion
+
+namespace DMT.Windows
+{
+    /// <summary>
+    /// Interaction logic for PlazaFundUpdateExchangeWindow.xaml
+    /// </summary>
+    public partial class PlazaFundUpdateExchangeWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PlazaFundUpdateExchangeWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}

8b3c35f8a67f6dd9e26f0d21154518cfc16e4873
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-09 04:16:05 +0700

    Update 2020-06-09 #4 - Add SignInPage (page). - Add TA MainMenu (page). - Add TOD MainMenu (page).

---
+++ 02.Controls/01.DMT.Controls/Controls/SignIn/Pages/SignInPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+using NLib;
+using NLib.Services;
+
+#endregion
+
+namespace DMT.Pages
+{
+    /// <summary>
+    /// Interaction logic for SignInPage.xaml
+    /// </summary>
+    public partial class SignInPage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public SignInPage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
---
+++ 02.Controls/02.DMT.Pages/TA/Menu/MainMenu.xaml.cs
@@ -0,0 +1,33 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+using NLib;
+using NLib.Services;
+
+#endregion
+
+namespace DMT.TA.Pages
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}

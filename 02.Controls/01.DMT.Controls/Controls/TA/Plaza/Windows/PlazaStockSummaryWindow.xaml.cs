c7097b976e840f1c3669b1b990efc29e00b31989
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-08 22:09:11 +0700

    Update 2020-06-08 #12 - Add TA - PlazaStockSummaryWindow (window).

---
+++ 02.Controls/01.DMT.Controls/Controls/TA/Plaza/Windows/PlazaStockSummaryWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+#endregion
+
+namespace DMT.Windows
+{
+    /// <summary>
+    /// Interaction logic for PlazaStockSummaryWindow.xaml
+    /// </summary>
+    public partial class PlazaStockSummaryWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PlazaStockSummaryWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}

789e262900557b709f7c20ee15a464d35eee7263
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-09 05:49:12 +0700

    Update 2020-06-09 #7 - DMT.Plaza.Config app project. Remove ribbon controls (has bug in RibbonWindow Title).

--- 09.App/04.DMT.Plaza.Config/MainWindow.xaml.cs
+++ 09.App/04.DMT.Plaza.Config/MainWindow.xaml.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#region Using
+
+using System;
+using System.Security.Principal;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+using NLib.Services;
+
+//using DMT.Models;
+//using DMT.Services;
+
+#endregion
 
 namespace DMT
 {

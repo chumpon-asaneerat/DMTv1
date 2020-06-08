b1d07acb7f74ddf9370cf3a76bdd7ab96f9a7b8b
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-08 21:51:02 +0700

    Update 2020-06-08 #9 - Add TA - CollectorFundBorrowWindow (window). - Add TA - CollectorFundReturnWindow (window).

---
+++ 02.Controls/01.DMT.Controls/Controls/TA/Collector/Fund/Windows/CollectorFundBorrowWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+#endregion
+
+namespace DMT.Windows
+{
+    /// <summary>
+    /// Interaction logic for CollectorFundBorrowWindow.xaml
+    /// </summary>
+    public partial class CollectorFundBorrowWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public CollectorFundBorrowWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
---
+++ 02.Controls/01.DMT.Controls/Controls/TA/Collector/Fund/Windows/CollectorFundReturnWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+#endregion
+
+namespace DMT.Windows
+{
+    /// <summary>
+    /// Interaction logic for CollectorFundReturnWindow.xaml
+    /// </summary>
+    public partial class CollectorFundReturnWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public CollectorFundReturnWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}

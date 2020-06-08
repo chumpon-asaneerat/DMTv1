f72e98a5e78a428bc3d342cbcbb4d04b1d6807b6
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-08 22:16:19 +0700

    Update 2020-06-08 #13 - Add TA - PlazaReceivedFundWindow (window).

---
+++ 02.Controls/01.DMT.Controls/Controls/TA/Plaza/Windows/PlazaReceivedFundWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+#endregion
+
+namespace DMT.Windows
+{
+    /// <summary>
+    /// Interaction logic for PlazaReceivedFundWindow.xaml
+    /// </summary>
+    public partial class PlazaReceivedFundWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PlazaReceivedFundWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}

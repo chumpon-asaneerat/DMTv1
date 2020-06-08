6444e6264725a78fb30b67532a2f377fcc867e23
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-08 22:20:31 +0700

    Update 2020-06-08 #14 - Add SignIn - SignInWindow (window).

---
+++ 02.Controls/01.DMT.Controls/Controls/SignIn/Windows/SignInWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+#endregion
+
+namespace DMT.Windows
+{
+    /// <summary>
+    /// Interaction logic for SignInWindow.xaml
+    /// </summary>
+    public partial class SignInWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public SignInWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}

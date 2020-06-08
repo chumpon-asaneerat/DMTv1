86f316c3fb3e0b091c55d35541f71b5985570e2f
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-08 22:05:47 +0700

    Update 2020-06-08 #11 - Add TOD - BOJWindow (window). - Add TOD - EOJWindow (window).

---
+++ 02.Controls/01.DMT.Controls/Windows/BOJWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+
+#endregion
+
+namespace DMT.Windows
+{
+    /// <summary>
+    /// Interaction logic for BOJWindow.xaml
+    /// </summary>
+    public partial class BOJWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public BOJWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ 02.Controls/01.DMT.Controls/Windows/EOJWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+
+#endregion
+
+namespace DMT.Windows
+{
+    /// <summary>
+    /// Interaction logic for EOJWindow.xaml
+    /// </summary>
+    public partial class EOJWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public EOJWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}

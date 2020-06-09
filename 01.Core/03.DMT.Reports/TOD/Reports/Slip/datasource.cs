b31c2e4faf2e2d50805605acbb086006ab2bc1c5
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-09 21:15:03 +0700

    Update 2020-06-09 #13 - Add sample structure for report datasource (criteria and item)/report file.

---
+++ 01.Core/03.DMT.Reports/TOD/Reports/Slip/datasource.cs
@@ -0,0 +1,57 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+
+using NLib.Reports.Rdlc;
+
+#endregion
+
+namespace DMT.TOD.Reports.Slip
+{
+    /// <summary>
+    /// Revenue Slip Criteria class.
+    /// </summary>
+    public class SlipCriteria : RdlcReportCriteria
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public SlipCriteria() : base()
+        {
+        }
+
+        #endregion
+
+        #region Override Methods
+
+        /// <summary>
+        /// Verify.
+        /// </summary>
+        /// <returns>Retruns true if criteria is valid.</returns>
+        public override bool Verify()
+        {
+            return true;
+        }
+
+        #endregion
+    }
+    /// <summary>
+    /// Revenue Slip Item class.
+    /// </summary>
+    public class SlipItem 
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public SlipItem() : base()
+        {
+        }
+
+        #endregion
+    }
+}

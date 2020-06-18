567f44ad9039e31821ac6960c90e0ad54741cb18
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-18 19:49:15 +0700

    Update 2020-06-18 #17 - Update ReflectionSample project #2.

--- 10.Tests/16.ReflectionSample/Form1.Designer.cs
+++ 10.Tests/16.ReflectionSample/Form1.Designer.cs
@@ -28,20 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.propertyGrid2 = new System.Windows.Forms.PropertyGrid();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Location = new System.Drawing.Point(12, 22);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(332, 394);
+            this.propertyGrid1.TabIndex = 0;
+            // 
+            // propertyGrid2
+            // 
+            this.propertyGrid2.Location = new System.Drawing.Point(498, 22);
+            this.propertyGrid2.Name = "propertyGrid2";
+            this.propertyGrid2.Size = new System.Drawing.Size(332, 394);
+            this.propertyGrid2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(364, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = ">>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(364, 191);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "<<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(849, 430);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.propertyGrid2);
+            this.Controls.Add(this.propertyGrid1);
             this.Name = "Form1";
             this.Text = "Reflection Sample";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.PropertyGrid propertyGrid2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- 10.Tests/16.ReflectionSample/Form1.cs
+++ 10.Tests/16.ReflectionSample/Form1.cs
@@ -4,9 +4,13 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using System.Reflection;
+using NLib.Reflection;
 
 namespace ReflectionSample
 {
@@ -15,6 +19,100 @@ namespace ReflectionSample
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private A objA = new A();
+        private B objB = new B();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            propertyGrid1.SelectedObject = objA;
+            propertyGrid2.SelectedObject = objB;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // A->B
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // B->A
+        }
+    }
+
+
+    public class A
+    {
+        [MapTo("Text")]
+        public string Text { get; set; }
+        [MapTo("Age")]
+        public int Age { get; set; }
+        [MapTo("DOB")]
+        public DateTime DOB { get; set; }
+    }
+
+
+    public class B
+    {
+        [MapTo("Text")]
+        public string MyText { get; set; }
+        [MapTo("Age")]
+        public int MyAge { get; set; }
+        [MapTo("DOB")]
+        public DateTime Dob { get; set; }
+    }
+
+    public class MapToAttribute : Attribute
+    {
+        private MapToAttribute() : base() { }
+        public MapToAttribute(string name) : base()
+        {
+            this.Name = name;
+        }
+        public string Name { get; set; }
+    }
+
+    public static class AssignExtensionMethods
+    {
+        private static Dictionary<string, Dictionary<Type, string>> _caches = new Dictionary<string, Dictionary<Type, string>>();
+
+        public static void InitType(Type type)
+        {
+            PropertyInfo[] props = type.GetProperties(
+                BindingFlags.Public | BindingFlags.IgnoreCase);
+            foreach (var prop in props)
+            {
+                MapToAttribute map = prop.GetCustomAttribute<MapToAttribute>(true);
+                if (null != map)
+                {
+                    Dictionary<Type, string> typeProps;
+                    if (!_caches.ContainsKey(map.Name))
+                    {
+                        typeProps = new Dictionary<Type, string>();
+                        _caches.Add(map.Name, typeProps);
+                    }
+                    else typeProps = _caches[map.Name];
+
+                    if (!typeProps.ContainsKey(type))
+                    {
+                        typeProps.Add(type, prop.Name);
+                    }
+                }
+            }
+        }
+
+
+        public static void AssignTo<TSource, TTarget>(this TSource source, TTarget target)
+        {
+            if (null == source || null == target) return;
+            Type scrType = typeof(TSource);
+            Type dstType = typeof(TTarget);
+            //NLib.Reflection.PropertyAccess;
+            InitType(scrType);
+            InitType(dstType);
+            //_caches
+            //MapToAttribute[] maps = scrType.GetCustomAttributes<MapToAttribute>(true).ToArray();
         }
     }
 }

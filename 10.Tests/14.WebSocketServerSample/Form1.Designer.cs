c84ae97353aa593d925ae465f3b079e758a223c2
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-18 12:48:31 +0700

    Update 2020-06-18 #2 - Update WebSocketServerSample project #2. - Update WebSocketClientSample project #2.

--- 10.Tests/14.WebSocketServerSample/Form1.Designer.cs
+++ 10.Tests/14.WebSocketServerSample/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Location = new System.Drawing.Point(12, 22);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(13, 17);
+            this.lbStatus.TabIndex = 0;
+            this.lbStatus.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(457, 279);
+            this.Controls.Add(this.lbStatus);
+            this.Name = "Form1";
+            this.Text = "Web Socket Server";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbStatus;
     }
 }
 
--- 10.Tests/15.WebSocketClientSample/Form1.cs
+++ 10.Tests/15.WebSocketClientSample/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using WebSocketSharp;
+
 namespace WebSocketClientSample
 {
     public partial class Form1 : Form
@@ -15,6 +17,78 @@ namespace WebSocketClientSample
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private int portNo = 4000;
+        private WebSocket ws = null;
+
+        private void Connect(string name)
+        {
+            if (string.IsNullOrEmpty(name.Trim())) return;
+            if (null == ws)
+            {
+                ws = new WebSocket(string.Format("ws://localhost:{0}/Chat", portNo));
+                ws.OnMessage += Ws_OnMessage;
+                ws.Connect();
+                Send(name + " say hello");
+            }
+        }
+
+        private void Disconnect()
+        {
+            if (null != ws)
+            {
+                ws.OnMessage -= Ws_OnMessage;
+                ws.Close();
+            }
+            ws = null;
+        }
+
+        private void Send(string message)
+        {
+            if (null == ws || ws.ReadyState != WebSocketState.Open) return;
+            ws.Send(message);
+        }
+
+        private void Ws_OnMessage(object sender, MessageEventArgs e)
+        {
+            // Cross thread wrapper.
+            Invoke(new MethodInvoker(delegate () {
+                string message = e.Data;
+                ListViewItem item = new ListViewItem();
+                item.Text = DateTime.Now.ToString("HH:mm:ss");
+                item.SubItems.Add(message);
+                lvMessages.Items.Add(item);
+            }));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Disconnect();
+        }
+
+        private void cmdConnect_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            Connect(name);
+        }
+
+        private void cmdDisconnect_Click(object sender, EventArgs e)
+        {
+            Disconnect();
+        }
+
+        private void cmdSend_Click(object sender, EventArgs e)
+        {
+            string message = txtMessage.Text;
+            if (string.IsNullOrEmpty(message.Trim())) return;
+            txtMessage.Text = string.Empty; // clear message.
+            Send(message);
         }
     }
 }

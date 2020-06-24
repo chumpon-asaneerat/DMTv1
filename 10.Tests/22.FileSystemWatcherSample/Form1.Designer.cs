33be3a0a5ea058102f3c97731f72f303cb5657d2
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-24 22:40:35 +0700

    Update 2020-06-24 #11 - Update FileSystemWatcher project #2 - simple watch json files.

--- 10.Tests/22.FileSystemWatcherSample/Form1.Designer.cs
+++ 10.Tests/22.FileSystemWatcherSample/Form1.Designer.cs
@@ -28,20 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lstMsgs = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // lstMsgs
+            // 
+            this.lstMsgs.FormattingEnabled = true;
+            this.lstMsgs.IntegralHeight = false;
+            this.lstMsgs.ItemHeight = 16;
+            this.lstMsgs.Location = new System.Drawing.Point(12, 12);
+            this.lstMsgs.Name = "lstMsgs";
+            this.lstMsgs.Size = new System.Drawing.Size(627, 408);
+            this.lstMsgs.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(651, 424);
+            this.Controls.Add(this.lstMsgs);
             this.Name = "Form1";
             this.Text = "Watch File(s)";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstMsgs;
     }
 }
 
--- 10.Tests/22.FileSystemWatcherSample/Form1.cs
+++ 10.Tests/22.FileSystemWatcherSample/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,72 @@ namespace FileSystemWatcherSample
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private FileSystemWatcher watcher;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lstMsgs.Items.Clear();
+
+            string root = Path.GetDirectoryName(this.GetType().Assembly.Location);
+            string path = Path.Combine(root, "data");
+            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+            watcher = new FileSystemWatcher(path, "*.json");
+
+            watcher.Created += Watcher_Created;
+            watcher.Renamed += Watcher_Renamed;
+            watcher.Changed += Watcher_Changed;
+            watcher.Deleted += Watcher_Deleted;
+            watcher.Error += Watcher_Error;
+            watcher.EnableRaisingEvents = true; 
+        }
+
+        private void UpdateMessage(string message)
+        {
+            Invoke(new Action(() =>
+            {
+                lstMsgs.Items.Add(message);
+            }));            
+        }
+
+        private void Watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            UpdateMessage(string.Format("Created: {0}", e.Name));
+        }
+
+        private void Watcher_Renamed(object sender, RenamedEventArgs e)
+        {
+            UpdateMessage(string.Format("Renamed: {0} to {1}", e.OldName, e.Name));
+        }
+
+        private void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            UpdateMessage(string.Format("Changed: {0}", e.Name));
+        }
+
+        private void Watcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            UpdateMessage(string.Format("Deleted: {0}", e.Name));
+        }
+
+        private void Watcher_Error(object sender, ErrorEventArgs e)
+        {
+            UpdateMessage(string.Format("Error: {0}", e.ToString()));
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (null != watcher)
+            {
+                watcher.Error -= Watcher_Error;
+                watcher.Deleted -= Watcher_Deleted;
+                watcher.Changed -= Watcher_Changed;
+                watcher.Renamed -= Watcher_Renamed;
+                watcher.Created -= Watcher_Created;
+                watcher.Dispose();
+            }
+            watcher = null;
         }
     }
 }

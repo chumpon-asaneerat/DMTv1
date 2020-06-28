7937463f8c3edb9f961817bc960630f0f222253a
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-29 06:16:05 +0700

    Update 2020-06-29 #1 - Update NQuery and NQuery<T> class #1.

--- 02.Models/02.DMT.Data.Services/Models/DataModels.cs
+++ 02.Models/02.DMT.Data.Services/Models/DataModels.cs
@@ -4345,7 +4345,7 @@ namespace DMT.Models.Domains
 
         #endregion
 
-        #region Used Default Connection
+        #region Used Custom Connection
 
         /// <summary>
         /// Checks is item is already exists in database.
@@ -4581,15 +4581,23 @@ namespace DMT.Models.Domains
 
     #endregion
 
-
     #region NQuery
 
     /// <summary>
     /// The NQuery abstract class.
     /// </summary>
-    public abstract class NQuery
+    public abstract class NQuery : DMTModelBase
     {
-        #region Static Methods
+        #region Static Variables and Properties
+
+        /// <summary>
+        /// sync object used for lock concurrent access.
+        /// </summary>
+        protected static object sync = new object();
+        /// <summary>
+        /// Gets default Connection.
+        /// </summary>
+        public static SQLiteConnection Default { get; set; }
 
         #endregion
     }
@@ -4602,9 +4610,43 @@ namespace DMT.Models.Domains
     /// The NQuery (Generic) abstract class.
     /// </summary>
     /// <typeparam name="T">The Target Class.</typeparam>
-    public abstract class NQuery<T>
+    public abstract class NQuery<T> : NQuery
+        where T : NQuery, new()
     {
         #region Static Methods
+
+        /// <summary>
+        /// Query.
+        /// </summary>
+        /// <param name="db">The connection.</param>
+        /// <param name="query">The query string.</param>
+        /// <param name="args">The query arguments.</param>
+        /// <returns>Returns query result in List.</returns>
+        public static List<T> Query(SQLiteConnection db, string query, params object[] args)
+        {
+            lock (sync)
+            {
+                List<T> results = null;
+                if (null == db || string.IsNullOrEmpty(query)) return results;
+                // execute query.
+                results = db.Query<T>(query, args).ToList();
+                return results;
+            }
+        }
+        /// <summary>
+        /// Query.
+        /// </summary>
+        /// <param name="query">The query string.</param>
+        /// <param name="args">The query arguments.</param>
+        /// <returns>Returns query result in List.</returns>
+        public static List<T> Query(string query, params object[] args)
+        {
+            lock (sync)
+            {
+                SQLiteConnection db = Default;
+                return Query(query, args);
+            }
+        }
 
         #endregion
     }

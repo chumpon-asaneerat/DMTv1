f3140dccff026115d95225f8fe02e0c666c009f2
chumpon-asaneerat <chumponsenate@gmail.com>
2020-07-02 19:27:42 +0700

    Update 2020-07-02 #3 - Smartcard API reimplements #7.

--- 10.Tests/21.DMTSmartCardSample/Form1.Designer.cs
+++ 10.Tests/21.DMTSmartCardSample/Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.lbCardExist = new System.Windows.Forms.Label();
+            this.lstDevices = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
             // label1
@@ -50,11 +53,38 @@
             this.lbCardExist.TabIndex = 1;
             this.lbCardExist.Text = "No card.";
             // 
+            // lstDevices
+            // 
+            this.lstDevices.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lstDevices.FormattingEnabled = true;
+            this.lstDevices.IntegralHeight = false;
+            this.lstDevices.ItemHeight = 16;
+            this.lstDevices.Location = new System.Drawing.Point(12, 57);
+            this.lstDevices.Name = "lstDevices";
+            this.lstDevices.Size = new System.Drawing.Size(789, 339);
+            this.lstDevices.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Devices:";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(565, 338);
+            this.ClientSize = new System.Drawing.Size(813, 408);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstDevices);
             this.Controls.Add(this.lbCardExist);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -70,6 +100,9 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lbCardExist;
+        private System.Windows.Forms.ListBox lstDevices;
+        private System.Windows.Forms.Label label2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- 10.Tests/21.DMTSmartCardSample/Form1.cs
+++ 10.Tests/21.DMTSmartCardSample/Form1.cs
@@ -11,8 +11,10 @@ using System.Windows.Forms;
 using System.Windows.Threading;
 
 using System.IO;
+
+using System.Management;
 using DMT.Smartcard;
-using System.Net.Http.Headers;
+
 
 
 namespace DMTSmartCardSample
@@ -24,13 +26,15 @@ namespace DMTSmartCardSample
             InitializeComponent();
         }
 
+        SL600SDKFactory factory = null;
         private SL600SDK sdk = null;
+
         private Sl600SmartCardReader reader = null;
         private DispatcherTimer timer = null;
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            var factory = SL600SDKFactory.CreateFactory(
+            factory = SL600SDKFactory.CreateFactory(
                 Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "MasterRD.dll"));
             //var resolver = CreateResolver();
             sdk = factory.CreateInstance();
@@ -38,8 +42,11 @@ namespace DMTSmartCardSample
 
             timer = new DispatcherTimer();
             timer.Tick += Timer_Tick;
-            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Interval = TimeSpan.FromMilliseconds(200);
             timer.Start();
+
+            //backgroundWorker1.RunWorkerAsync();
+            Scan();
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
@@ -58,23 +65,91 @@ namespace DMTSmartCardSample
             sdk = null;
         }
 
+        private bool onScanning = false;
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            if (null == reader) return;
+            if (onScanning) return;
 
-            if (reader.IsCardExist())
+            onScanning = true;
+
+            if (null != reader)
             {
-                lbCardExist.Text = "Card dected.";
-                lbCardExist.ForeColor = Color.ForestGreen;
-            }
-            else
-            {
-                lbCardExist.Text = "No card.";
-                lbCardExist.ForeColor = Color.Red;
+                if (reader.IsCardExist())
+                {
+                    lbCardExist.Text = "Card dected.";
+                    lbCardExist.ForeColor = Color.ForestGreen;
+                }
+                else
+                {
+                    lbCardExist.Text = "No card.";
+                    lbCardExist.ForeColor = Color.Red;
+                }
+
+                Application.DoEvents();
             }
 
-            Application.DoEvents();
+            onScanning = false;
+        }
+
+        /*
+        private void DeviceInsertedEvent(object sender, EventArrivedEventArgs e)
+        {
+            ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+            foreach (var property in instance.Properties)
+            {
+                Console.WriteLine(property.Name + " = " + property.Value);
+            }
+        }
+
+        private void DeviceRemovedEvent(object sender, EventArrivedEventArgs e)
+        {
+            ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+            foreach (var property in instance.Properties)
+            {
+                Console.WriteLine(property.Name + " = " + property.Value);
+            }
+        }
+        */
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            /*
+            WqlEventQuery insertQuery = new WqlEventQuery("SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'");
+
+            ManagementEventWatcher insertWatcher = new ManagementEventWatcher(insertQuery);
+            insertWatcher.EventArrived += new EventArrivedEventHandler(DeviceInsertedEvent);
+            insertWatcher.Start();
+
+            WqlEventQuery removeQuery = new WqlEventQuery("SELECT * FROM __InstanceDeletionEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'");
+            ManagementEventWatcher removeWatcher = new ManagementEventWatcher(removeQuery);
+            removeWatcher.EventArrived += new EventArrivedEventHandler(DeviceRemovedEvent);
+            removeWatcher.Start();
+            */
+        }
+
+        private void Scan()
+        {
+            /*
+            string ComputerName = "localhost";
+            ManagementScope Scope;
+            Scope = new ManagementScope(String.Format("\\\\{0}\\root\\CIMV2", ComputerName), null);
+
+            Scope.Connect();
+
+            string query = "SELECT * FROM Win32_PnPEntity Where ClassGuid = NULL";
+
+            ObjectQuery Query = new ObjectQuery(query);
+
+            ManagementObjectSearcher Searcher = new ManagementObjectSearcher(Scope, Query);
+
+            lstDevices.Items.Clear();
+            foreach (ManagementObject WmiObject in Searcher.Get())
+            {
+                //textBox1.Text += WmiObject["Name"] + "\r\n";
+                if (null != WmiObject["Name"])
+                    lstDevices.Items.Add(WmiObject["Name"]);
+            }
+            */
         }
     }
 }
